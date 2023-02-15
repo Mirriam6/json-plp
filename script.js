4abf334104cf9474ca656150d9c941bcf1cd6597
mirriam6 <mirriammlaliki640@gmail.com>
2023-02-15 13:49:57 +0200

    my first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const data={
+    "name": "John",
+    "age": "22",
+    "hobby": {
+        "reading": true,
+        "gaming": false,
+        "sports": "football"
+    },
+    "class": ["javascript", "html", "css"]
+}
+
+
+console.log(data.name);
+console.log(data.hobby);
+console.log(data.hobby.sports);
+console.log(data.class[1]);
